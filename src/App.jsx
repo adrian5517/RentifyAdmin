5eaf4494384bdd1e22e2166881a0e6da758f39bb
Adrian Boncodin <juelboncodin@gmail.com>
2025-07-09 18:39:31 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="text-3xl font-bold underline">
+            Welcome to Rentify Admin
+          </h1>
+        </header>
+      </div>
+      <div className="bg-blue-500 text-white p-4">
+        This is a Tailwind CSS styled div.
+      </div>
+    </>
+  )
+}
+
+export default App
