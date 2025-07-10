7530d995f8636af6adc8d2c5c9250a108372de95
Adrian Boncodin <juelboncodin@gmail.com>
2025-07-10 18:54:04 +0800

    Already Route the pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@
 import './App.css'
 import {BrowserRouter , Routes , Route } from 'react-router-dom'
 import Home from './pages/Home'
-
+import LoginPage from './pages/LoginPage'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import Skills from './pages/Skills'
 function App() {
   
 
@@ -12,6 +16,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
         
       </Routes>
     </BrowserRouter>
